00547499ff6143731824dc81c9eea6d1be51a4cb
audusunday <sunnadu13@gmail.com>
2023-12-06 04:15:10 +0100

    weak link data

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,11 @@
-export default function updateUniqueItems(map) {
-  if (map instanceof Map) {
-    for (const [key, value] of map) {
-      if (value === 1) {
-        map.set(key, 100);
-      }
-    }
-  } else {
-    throw new Error('Cannot process');
+export const weakMap = new WeakMap();
+let count = 1;
+
+export function queryAPI(endPoint) {
+  weakMap.set(endPoint, count);
+  count += 1;
+  const queryCount = weakMap.get(endPoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
   }
-  return map;
 }
